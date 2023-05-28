1be128774e1e015dfdc430f8bebdcf5403d93e9c
Manikandan310 <89017595+Manikandan310@users.noreply.github.com>
2023-05-28 13:26:07 +0530

    Buttons.java

--- day2/Buttons.java
+++ day2/Buttons.java
@@ -9,30 +9,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Buttons {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://www.leafground.com/button.xhtml");
 		driver.manage().window().maximize();
-
-		// Confirm title
 		
 		WebElement title = driver.findElement(By.id("j_idt88:j_idt90"));
 		title.click();
 		driver.get("http://www.leafground.com/button.xhtml");
 		
-		// button is disabled.
-		
 		boolean button = driver.findElement(By.id("j_idt88:j_idt92")).isEnabled();
 		System.out.println(button);
-		
-		// Submit button
-		
+	
 		WebElement submitButton = driver.findElement(By.id("j_idt88:j_idt94"));
 		org.openqa.selenium.Point Location = submitButton.getLocation();
 		System.out.println(Location);
-		
-		//height and width
+	
 		
 		org.openqa.selenium.Dimension size = driver.findElement(By.id("j_idt88:j_idt98")).getSize();
 		System.out.println(size);
